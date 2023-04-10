c14de9cf0118ab061bf3e4419e800faa12c4248f
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2023-04-09 20:46:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+async function getMenu() {
+    const client = contentful.createClient({
+        space: 'rmkbw43wse32',
+        accessToken: 'LH1A4Pbn5WMso-OgGWFmnBje0LY48PXd3d3rKLEsQ5c'
+    })
+    const res = await client.getEntries({ content_type: 'lilBaghdad' })
+    const data = await res.items;
+    const items = data.map(item => {
+        const { category, name, description, price } = item.fields;
+        const image = item.fields.image.fields.file.url;
+        return { category, name, description, price, image };
+    })
+    return items;
+}
+
+async function initApp() {
+    createLoader();
+    try {
+        const data = await getMenu();
+        createMenu(data);
+        onTabClick();
+    } catch {
+        document.querySelector('body').innerHTML = `
+        <h1>Ooops! We're having trouble loading the menu</h1>
+        `;
+    }
+}
+
+function createLoader() {
+    const loader = document.querySelector('aside');
+    setTimeout(() => {
+        loader.classList.remove('visible');
+        loader.innerHTML = '';
+    }, 2000);
+}
+
+function createCategories(menu) {
+    const appetizers = menu.filter(item => item.category === 'Appetizers');
+    const dishes = menu.filter(item => item.category === 'Dishes');
+    const breakfast = menu.filter(item => item.category === 'Breakfast');
+    const desserts = menu.filter(item => item.category === 'Desserts');
+    const drinks = menu.filter(item => item.category === 'Drinks');
+    return [appetizers, dishes, breakfast, desserts, drinks];
+}
+
+function createArticles() {
+    const labels = ['Appetizers', 'Dishes', 'Breakfast', 'Desserts', 'Drinks'];
+    const subLabel = 'All Day, Saturdays Only';
+    const subLabels = ['Main dishes served with Pita bread', 'All day, Saturdays only'];
+    const articles = document.querySelectorAll('article');
+    const subArticles = [articles[1], articles[2]];
+    articles.forEach((article, i) => {
+        const h2 = document.createElement('h2');
+        const ul = document.createElement('ul');
+        h2.innerText = labels[i];
+        article.appendChild(h2);
+        article.appendChild(ul);
+    });
+    subArticles.forEach((article, i) => {
+        const h2 = article.querySelector('h2');
+        const h3 = document.createElement('h3');
+        h3.innerText = subLabels[i];
+        h2.insertAdjacentElement('afterend', h3);
+    })
+}
+
+function createMenu(data) {
+    createArticles();
+    const categories = createCategories(data);
+    const ul = document.querySelectorAll('article ul');
+    categories.map((items, i) => {
+        ul[i].innerHTML += items.map(item => {
+            const { category, name, description, price, image } = item;
+            return `
+            <li onclick="createModal('${category}', '${name}', '${description}', '${price}', '${image}')">
+                <div class="item-image">
+                    <img src="${image}" alt="${name}" style="aspect-ratio: 16 / 10" loading="lazy">
+                </div>
+                <div class="item-details">
+                    <h3>${name}</h3>
+                    <p>${description}</p>
+                    <h4>${price}</h4>
+                </div>
+            </li>
+            `
+        }).join('');
+    })
+}
+
+function createModal(category, name, description, price, image) {
+    const body = document.querySelector('body');
+    const dialog = document.querySelector('dialog');
+    body.style.overflow = 'hidden';
+    dialog.classList.add('expanded');
+    dialog.setAttribute('aria-hidden', 'false');
+    dialog.innerHTML += `
+    <div class="dialog-scrim"></div>
+    <div class="dialog-container">
+        <div class="dialog-header" style="background-image: url('${image}')">
+            <button type="button" aria-label="Close menu item" onclick="closeModal()"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="16" height="16" fill="#292a2d"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></button>
+        </div>
+        <div class="dialog-body">
+            <h5>${category}</h5>
+            <h3>${name}</h3>
+            <p>${description}</p>
+            <h4>${price}</h4>
+        </div>
+    </div>
+    `
+}
+
+function closeModal() {
+    const body = document.querySelector('body');
+    const dialog = document.querySelector('dialog');
+    body.style.overflow = 'auto';
+    dialog.classList.remove('expanded');
+    dialog.setAttribute('aria-hidden', 'true');
+    dialog.innerHTML = '';
+}
+
+function onTabClick() {
+    const tabs = document.querySelectorAll('nav ul li');
+    tabs.forEach((tab, i) => {
+        tab.addEventListener('click', () => {
+            tabs.forEach(tab => tab.classList.remove('active'));
+            tabs[i].classList.add('active');
+        })
+    })
+}
+
+function onPageScroll() {
+    const articles = document.querySelectorAll('article');
+    const tabs = document.querySelectorAll('nav ul li');
+    let current = '';
+    articles.forEach(article => {
+        const articleTop = article.offsetTop;
+        if (window.pageYOffset >= articleTop - 50) {
+            current = article.getAttribute('id');
+        }
+    })
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+        if (tab.classList.contains(current)) {
+            tab.classList.add('active');
+        }
+    })
+}
+
+window.addEventListener('scroll', onPageScroll);
+
+initApp()
